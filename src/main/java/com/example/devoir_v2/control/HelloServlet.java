35ac6e45f10b4789a46bebf35fe523ecfcb7ad53
Christophe <arlasam14@gmail.com>
2024-05-14 14:44:24 -0400

    Restructure du projet

---
+++ src/main/java/com/example/devoir_v2/control/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.example.devoir_v2.control;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/page2")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+
+
+}
